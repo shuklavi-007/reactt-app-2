1d73c3e5af566624a57562ad3aafebdd39a5ef1c
Shukla <avinash.shukla@hp.com>
2021-04-05 00:12:32 +0530

    left-Right button added in Array

--- src/component/searchbox/SearchBox.jsx
+++ src/component/searchbox/SearchBox.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./SearchBox.scss";
 import { Button, Modal } from "react-bootstrap"
+import { faLessThanEqual } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -15,7 +16,7 @@ class SearchBox extends Component {
       taskArray3: [],
       taskArray4: [],
       show: false,
-      buttonId: ''
+      buttonId: '',
     };
   }
 
@@ -82,6 +83,98 @@ class SearchBox extends Component {
 
     }
   }
+  moveRight = (value, buttonID) => {
+    if (buttonID == 'button5') {
+      let input = this.state.taskArray2
+      input.push(value)
+      this.setState({
+        taskArray2: input,
+        value: '',
+      })
+      let oldValue = this.state.taskArray1
+      let index = oldValue.indexOf(value)
+      if (index !== -1) {
+        oldValue.splice(index, 1);
+      }
+    }
+    if (buttonID == 'button6') {
+      let input = this.state.taskArray3
+      input.push(value)
+      this.setState({
+        taskArray3: input,
+        value: '',
+      })
+      let oldValue = this.state.taskArray2
+      let index = oldValue.indexOf(value)
+      if (index !== -1) {
+        oldValue.splice(index, 1);
+      }
+    }
+
+    if (buttonID == 'button7') {
+      let input = this.state.taskArray4
+      input.push(value)
+      this.setState({
+        taskArray4: input,
+        value: '',
+      })
+      let oldValue = this.state.taskArray3
+      let index = oldValue.indexOf(value)
+      if (index !== -1) {
+        oldValue.splice(index, 1);
+      }
+    }
+
+
+  }
+  moveLeft = (value, button) => {
+    if (button == 'button6') {
+      let input = this.state.taskArray1
+      input.push(value)
+      this.setState({
+        taskArray1: input,
+        value: '',
+      })
+      let oldValue = this.state.taskArray2
+      let index = oldValue.indexOf(value)
+      if (index !== -1) {
+        oldValue.splice(index, 1);
+      }
+    }
+    if (button == 'button7') {
+      let input = this.state.taskArray2
+      input.push(value)
+      this.setState({
+        taskArray2: input,
+        value: '',
+      })
+      let oldValue = this.state.taskArray3
+      let index = oldValue.indexOf(value)
+      if (index !== -1) {
+        oldValue.splice(index, 1);
+      }
+    }
+
+    if (button == 'button8') {
+      let input = this.state.taskArray3
+      input.push(value)
+      this.setState({
+        taskArray3: input,
+        value: '',
+      })
+      let oldValue = this.state.taskArray4
+      let index = oldValue.indexOf(value)
+      if (index !== -1) {
+        oldValue.splice(index, 1);
+      }
+    }
+
+  }
+
+
+
+
+
 
 
   render() {
@@ -125,6 +218,8 @@ class SearchBox extends Component {
                   this.state.taskArray1.map((v, k) => {
                     return (
                       <div className="taskcard mt-2" key={k}>
+                        <button id="button5" onClick={() => this.moveRight(v, 'button5')}> moveRight </button>
+                        <button id="button5" onClick={() => this.moveLeft(v, 'button5')}> moveLeft </button>
                         {v}
                       </div>
 
@@ -139,6 +234,8 @@ class SearchBox extends Component {
                   this.state.taskArray2.map((v, k) => {
                     return (
                       < div className="taskcard mt-2" key={k} >
+                        <button id="button6" onClick={() => this.moveRight(v, 'button6')}> moveRight </button>
+                        <button id="button6" onClick={() => this.moveLeft(v, 'button6')}> moveLeft </button>
                         {v}
                       </div>
 
@@ -153,6 +250,8 @@ class SearchBox extends Component {
                   this.state.taskArray3.map((v, k) => {
                     return (
                       < div className="taskcard mt-2" key={k} >
+                        <button id="button7" onClick={() => this.moveRight(v, 'button7')}> moveRight </button>
+                        <button id="button7" onClick={() => this.moveLeft(v, 'button7')}> moveLeft </button>
                         {v}
                       </div>
 
@@ -168,6 +267,8 @@ class SearchBox extends Component {
                   this.state.taskArray4.map((v, k) => {
                     return (
                       < div className="taskcard mt-2" key={k} >
+                        <button id="button8" onClick={() => this.moveRight(v, 'button8')}> moveRight </button>
+                        <button id="button8" onClick={() => this.moveLeft(v, 'button8')}> moveLeft </button>
                         {v}
                       </div>
 
